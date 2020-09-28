4978751f1dc6603f6e2f9cefe86a60f1c6e0eb8c
Jack Cannon <CannonJack17@gmail.com>
2020-09-27 20:22:50 -0400

    commented card

--- app/src/main/java/com/jackcannon/blackjack/Card.java
+++ app/src/main/java/com/jackcannon/blackjack/Card.java
@@ -1,5 +1,14 @@
 package com.jackcannon.blackjack;
 
+/**
+ * Card Class
+ * This class builds a Card object to associate images with the characteristics of a card
+ * CS482 Software Engineering
+ * Dr. Raunak
+ * @author Jack Cannon
+ * @author Ian Gifford
+ * @version 1.0 Initial Build
+ */
 public class Card {
 
     String suit;
@@ -7,6 +16,13 @@ public class Card {
     int point_value;
     int image_resource;
 
+    /**
+     * Card Constructor
+     * @param suit the card suit (clubs, spades, hearts, diamonds)
+     * @param face_value the face value (1,2, Jack , King, etc.)
+     * @param point_value the integer value of a card
+     * @param image_resource the image associated with this card
+     */
     public Card(String suit, String face_value, int point_value, int image_resource) {
         this.suit = suit;
         this.face_value = face_value;
